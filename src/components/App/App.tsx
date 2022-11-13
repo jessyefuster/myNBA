25a8c9f2adcaee59fad5da96958fc970104fd294
Jessye Fuster <dev.fuster.jessye@gmail.com>
2022-11-13 19:20:35 +0100

    REFACTO(App) - Migrated to CSS module for styling

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,19 @@
 import logo from 'assets/images/logo.svg';
 import { useTranslation } from 'react-i18next';
-import './App.css';
+import styles from './App.module.css';
 
 const App = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className={styles.container}>
+      <header className={`background ${styles.header}`}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <p className="headline-large">
           {t('title')}
         </p>
         <a
-          className="App-link"
+          className={`title-large ${styles.link}`}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
