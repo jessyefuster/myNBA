2cfedcf9831c47d6db4a55b9482598ff5f1acfe0
Jessye Fuster <dev.fuster.jessye@gmail.com>
2022-11-20 16:00:49 +0100

    FEAT(Header) - Modified component so it actually is a header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,24 +1,13 @@
 import logo from 'assets/images/logo.svg';
-import { useTranslation } from 'react-i18next';
 import styles from './Header.module.css';
 
 const Header = () => {
-  const { t } = useTranslation();
-
   return (
-    <header className={`background ${styles.container}`}>
+    <header className={`${styles.container}`}>
       <img src={logo} className={styles.logo} alt="logo" />
-      <p className="headline-large">
-        {t('title')}
-      </p>
-      <a
-        className={`title-large ${styles.link}`}
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {t('learnReact')}
-      </a>
+      <h1 className="headline-small">
+        myNBA
+      </h1>
     </header>
   );
 }
