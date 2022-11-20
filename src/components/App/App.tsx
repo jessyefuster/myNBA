2460a3fed2ac98935de1d072ebfd0b99e6be73ea
Jessye Fuster <dev.fuster.jessye@gmail.com>
2022-11-20 16:03:23 +0100

    FEAT - Implemented routing with Home and default 404 pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,16 @@
 import Header from 'components/layout/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from 'pages/Home';
+import NotFound from 'pages/NotFound';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from 'components/App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 import 'utils/i18n';
 
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
