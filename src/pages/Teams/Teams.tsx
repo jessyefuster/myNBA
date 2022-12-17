aa1acb91db790768030c1104d4a6990688a4f5bc
Jessye Fuster <dev.fuster.jessye@gmail.com>
2022-12-17 21:21:45 +0100

    FEAT(Teams) - Basic teams fetch and display

--- src/pages/Teams/Teams.tsx
+++ src/pages/Teams/Teams.tsx
@@ -1,16 +1,37 @@
-import { useTranslation } from 'react-i18next';
-import styles from './Home.module.css';
+import { useEffect, useState } from 'react';
+import TeamsService, { Team } from 'api/services/Teams';
+import styles from './Teams.module.css';
 
-const Home = () => {
-  const { t } = useTranslation();
+const Teams = () => {
+  const [teams, setTeams] = useState<Team[]>([]);
+  const [error, setError] = useState<boolean>();
+
+  const loadTeams = async () => {
+    try {
+      const teams = await TeamsService.list();
+      setTeams(teams);
+    }
+    catch (e) {
+      setError(true);
+    }
+  }
+
+  useEffect(() => {
+    loadTeams();
+  }, []);
 
   return (
     <main className={`background ${styles.container}`}>
-      <p className="headline-large">
-        {t('title')}
-      </p>
+      {teams.map(team => (
+        <div key={team.teamId} className={styles.team}>
+          <img className={`logo ${styles.teamLogo}`} src={team.logoUrl} alt="team logo" />
+          <p className={`title-medium ${styles.teamName}`}>
+            {team.fullName}
+          </p>
+        </div>
+      ))}
     </main>
   );
 }
 
-export default Home;
+export default Teams;
