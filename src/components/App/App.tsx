d6c5f28d981fe30489fd27cd64ab78006476ab80
Jessye Fuster <dev.fuster.jessye@gmail.com>
2022-12-17 18:20:46 +0100

    FEAT(style) - Aligned MUI theme with custom material theme/tokens

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,69 @@
-import Header from 'components/layout/Header';
 import { Route, Routes } from 'react-router-dom';
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+} from '@mui/material/styles';
+import Header from 'components/layout/Header';
 import Home from 'pages/Home';
 import NotFound from 'pages/NotFound';
 
+const theme = extendTheme({
+  typography: {
+    fontFamily: 'Poppins',
+    h1: {
+      fontFamily: 'Montserrat',
+      fontWeight: 600,
+    },
+    h2: {
+      fontFamily: 'Montserrat',
+      fontWeight: 600,
+    },
+    h4: {
+      fontFamily: 'Montserrat',
+      fontWeight: 600,
+    },
+    h5: {
+      fontFamily: 'Montserrat',
+      fontWeight: 600,
+    },
+    h3: {
+      fontFamily: 'Montserrat',
+      fontWeight: 600,
+    },
+    h6: {
+      fontFamily: 'Montserrat',
+      fontWeight: 600,
+    },
+    button: {
+      fontFamily: 'Montserrat',
+      fontWeight: 500,
+    },
+    subtitle2: {
+      fontFamily: 'Raleway',
+      fontWeight: 500,
+    },
+    subtitle1: {
+      fontFamily: 'Raleway',
+      fontWeight: 500,
+    },
+    overline: {
+      fontFamily: 'Raleway',
+    },
+  },
+  shape: {
+    borderRadius: 10,
+  }
+});
+
 const App = () => {
   return (
-    <>
+    <CssVarsProvider theme={theme}>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </CssVarsProvider>
   );
 }
 
