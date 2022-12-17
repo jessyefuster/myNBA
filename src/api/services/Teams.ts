afe183a39e2ab68ef11f1c2f4f0adf0eae76cade
Jessye Fuster <dev.fuster.jessye@gmail.com>
2022-12-17 21:14:03 +0100

    FEAT(api) - Added basic TeamsService

---
+++ src/api/services/Teams.ts
@@ -0,0 +1,60 @@
+import client from '../client';
+
+// TODO: move types
+type Conference = 
+  | 'East'
+  | 'West';
+
+type EasternDivision = 
+  | 'Atlantic'
+  | 'Central'
+  | 'Southeast';
+
+type WesternDivision = 
+  | 'Northwest'
+  | 'Pacific'
+  | 'Southwest';
+
+type Division = EasternDivision | WesternDivision;
+
+export interface TeamDTO {
+  city: string,
+  altCityName: string,
+  fullName: string,
+  tricode: string,
+  teamId: string,
+  nickname: string,
+  urlName: string,
+  teamShortName: string,
+  confName: Conference,
+  divName: Division
+}
+
+export interface Team extends TeamDTO {
+  logoUrl: string
+}
+
+interface EasternTeam extends Team {
+  confName: 'East',
+  divName: EasternDivision
+}
+
+interface WesternTeam extends Team {
+  confName: 'East',
+  divName: WesternDivision
+}
+
+const getTeams = async () => {
+  const data = await client.get('teams.json').json<TeamDTO[]>();
+
+  return data.map<Team>(item => ({
+    ...item,
+    logoUrl: `https://cdn.nba.com/logos/nba/${item.teamId}/global/L/logo.svg`
+  }));
+};
+
+const TeamsService = {
+  list: getTeams
+}
+
+export default TeamsService;
